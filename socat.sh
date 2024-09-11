4671c3f2af2c618f921b85ee44bb694f3e0abbe6
wallacebrf <62187108+wallacebrf@users.noreply.github.com>
2024-09-11 12:11:18 -0500

    Add files via upload

---
+++ socat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+	
+current_date=$(date '+%F %X')
+echo "$current_date - socat process restarted for the day" >> /var/log/socat-500.log
+echo "$current_date - socat process restarted for the day" >> /var/log/socat-4500.log
+socat UDP4-LISTEN:500,reuseaddr,fork UDP6:ipv6.your-domain.com:500 >> /var/log/socat-500.log 2>&1 &
+socat UDP4-LISTEN:4500,reuseaddr,fork UDP6:ipv6.yourdomain.com:4500 >> /var/log/socat-4500.log 2>&1 &
